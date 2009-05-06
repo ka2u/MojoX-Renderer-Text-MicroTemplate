30430d4c4c96a72d58368028cc44d1134581106f
Kazuhiro Shibuya <stevenlabs@gmail.com>
2009-05-06 22:24:30 +0900

    Added main code.
    Just work in the code correctly.

--- lib/MojoX/Renderer/Text/MicroTemplate.pm
+++ lib/MojoX/Renderer/Text/MicroTemplate.pm
@@ -4,6 +4,34 @@ use strict;
 use warnings;
 our $VERSION = '0.01';
 
+use Text::MicroTemplate::File;
+
+sub build_handler {
+    my $class   = shift;
+    my $mojo    = shift;
+    my $handler = shift || 'epl';
+    $mojo->renderer->add_handler(
+        $handler => sub {
+            my ( $self, $c, $output ) = @_;
+
+            my $path = $c->stash->{template_path};
+            $self->{_mt_cache} ||= {};
+            my $mt = $self->{_mt_cache}->{$path};
+
+            unless ($mt) {
+                $mt = $self->{_mt_cache}->{$path}
+                    = Text::MicroTemplate::File->new;
+                $mt->{line_start} = '%';
+                $mt->{tag_start}  = '<%';
+                $mt->{tag_end}    = '%>';
+            }
+            ${$output} = ${ $mt->render_file( $path, $c ) };
+        }
+    );
+}
+
+
+
 1;
 __END__
 
@@ -15,15 +43,34 @@ MojoX::Renderer::Text::MicroTemplate -
 
   use MojoX::Renderer::Text::MicroTemplate;
 
+  sub startup {
+      $self = shift;
+
+      ...
+
+      MojoX::Renderer::Text::MicroTemplate->build_handler($self);
+  }
+
 =head1 DESCRIPTION
 
-MojoX::Renderer::Text::MicroTemplate is
+MojoX::Renderer::Text::MicroTemplate is Text::MicroTemplate renderer for Mojo.
+
+=head1 TEMPLATE SYNTAX
+
+MojoX::Renderer::Text::MicroTemplate is same syntax of Mojo::Template.
+Additional, output the result of expression with automatic escape.
+See also L<Text::MicroTemplate>;
+
+    <%= $expr %>
+    %= $expr
 
 =head1 AUTHOR
 
 Kazuhiro Shibuya E<lt>stevenlabs {at} gmail.comE<gt>
 
 =head1 SEE ALSO
+
+L<Mojo>, L<Text::MicroTemplate>
 
 =head1 LICENSE
 
