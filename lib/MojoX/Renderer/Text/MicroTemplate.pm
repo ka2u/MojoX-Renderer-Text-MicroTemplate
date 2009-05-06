8b765f11e116cbaca3686f8beeee84fef64761f4
Kazuhiro Shibuya <stevenlabs@gmail.com>
2009-05-06 13:45:18 +0900

    initial commit

---
+++ lib/MojoX/Renderer/Text/MicroTemplate.pm
@@ -0,0 +1,33 @@
+package MojoX::Renderer::Text::MicroTemplate;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+MojoX::Renderer::Text::MicroTemplate -
+
+=head1 SYNOPSIS
+
+  use MojoX::Renderer::Text::MicroTemplate;
+
+=head1 DESCRIPTION
+
+MojoX::Renderer::Text::MicroTemplate is
+
+=head1 AUTHOR
+
+Kazuhiro Shibuya E<lt>stevenlabs {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
